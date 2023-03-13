e45049cd63c4d546711500c32f1b55e89fce1d36
mark4z <mark4z@apache.org>
2023-03-13 14:10:46 +0800

    http2

--- java/src/main/java/com/example/springmock/SpringMockApplication.java
+++ java/src/main/java/com/example/springmock/SpringMockApplication.java
@@ -1,16 +1,12 @@
 package com.example.springmock;
 
-import org.conscrypt.Conscrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.security.Security;
 
 @SpringBootApplication
 public class SpringMockApplication {
 
     public static void main(String[] args) {
-        Security.insertProviderAt(Conscrypt.newProvider(), 1);
         SpringApplication.run(SpringMockApplication.class, args);
     }
 
