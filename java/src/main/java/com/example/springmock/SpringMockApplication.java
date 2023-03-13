07f18d1651c1fc02bdc2d8136faf01c48e5b9790
mark4z <mark4z@apache.org>
2023-03-13 14:06:29 +0800

    mvn

--- java/src/main/java/com/example/springmock/SpringMockApplication.java
+++ java/src/main/java/com/example/springmock/SpringMockApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.security.Security;
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringMockApplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "US"));
         Security.insertProviderAt(Conscrypt.newProvider(), 1);
         SpringApplication.run(SpringMockApplication.class, args);
     }
